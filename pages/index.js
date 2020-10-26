2ea7161046edd9fae9bb8fcaff359c479ac94ea7
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-10-26 20:31:01 +0000

    api routes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-
+import Link from 'next/link'
+import Date from '../components/date'
 
 // getStaticProps allows to collect data before render of page via static generation
 // getStaticProps runs only on server-side
@@ -29,13 +30,15 @@ export default function Home({allPostsData}) {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+           <li className={utilStyles.listItem} key={id}>
+           <Link href={`/posts/${id}`}>
+             {title}
+           </Link>
+           <br />
+           <small className={utilStyles.lightText}>
+             <Date dateString={date} />
+           </small>
+         </li>
           ))}
         </ul>
       </section>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,28 +1,41 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+        {postData.id}
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date}></Date>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 } 
 
+
+// getstaticpaths and getstaticprops should never be fetched - instead write server-side code within
+// this is because these functions are only ever called on the serverside and are not included in the JS bundle used in the browser
+ 
 export async function getStaticPaths(){
   const paths = getAllPostIds()
   return{
     paths, 
-    fallback: false
+    fallback: false // returns a 404 if pages are not found
   }
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
